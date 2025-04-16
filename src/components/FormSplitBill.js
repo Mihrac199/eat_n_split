ef6444e1db61de0746eed5b51356f34988fbe2b2
Mihrac199 <mihrac.comart1996@gmail.com>
2025-04-16 03:12:03 +0300

    Update

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function FormSplitBill({ selectedFriend }) {
+
+     const [bill, setBill] = useState(null);
+     const [paidByUser, setPaidByUser] = useState(null);
+     const [whoIsPlaying, setWhoIsPlaying] = useState("user");
+     const paidByFriend = bill ? bill - paidByUser : null;
 
      return (
 
@@ -7,18 +14,26 @@ export default function FormSplitBill({ selectedFriend }) {
                <h2>Split a bill with {selectedFriend.name}</h2>
 
                <label>► Bill value</label>
-               <input type="text" />
+               <input type="text"
+                    value={bill}
+                    onChange={e => setBill(+e.target.value)} />
 
                <label>► Your expense</label>
-               <input type="text" />
+               <input type="text"
+                    value={paidByUser}
+                    onChange={e => setPaidByUser(
+                         +e.target.value > bill ? paidByUser : +e.target.value)} />
 
                <label>► {selectedFriend.name}'s expense</label>
-               <input type="text" disabled />
+               <input type="text" disabled value={paidByFriend} />
 
                <label>► Who is paying the bill</label>
-               <select>
+               <select value={whoIsPlaying}
+                    onChange={e => setWhoIsPlaying(e.target.value)}>
+
                     <option value="user">You</option>
                     <option value="friend">{selectedFriend.name}</option>
+
                </select>
 
           </form>
